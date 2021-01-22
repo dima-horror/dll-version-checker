77d714e115c0fc38313a6fec1ae184977e414ebc
Dima <dima.horror+gitlab@gmail.com>
2021-01-22 17:25:52 +0200

    Base load

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0 || String.IsNullOrEmpty(args[0]))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please, pass path to dll as parameter");
+                Console.ResetColor();
+                return;
+            }
+            Assembly assembly = Assembly.LoadFrom(args[0]);
+            Version ver = assembly.GetName().Version;
+            Console.WriteLine(ver?.ToString());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dll-version-checker
+
+Easy way to get c# DLL version
+
+# <a id="raw-url" href="https://raw.githubusercontent.com/dima-horror/dll-version-checker/master/dll-version.exe">Download</a> checker for windows.
+
+Usage:
+
+```
+dll-version "System.ComponentModel.Annotations.dll"
+```
